b1b932a969acfdb96697169106278b8ecb8a6eab
storres86 <hbkjones23@yahoo.com>
2020-11-03 20:39:07 -0800

    Updates

---
+++ precache-manifest.6a7e66a15c9ede84b9955439861dda98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344277b7fdd845a612d6e94d4ab20d75",
+    "url": "/jiujitsutimer/index.html"
+  },
+  {
+    "revision": "7d7b787452f161c7084f",
+    "url": "/jiujitsutimer/static/css/main.9bc324b5.chunk.css"
+  },
+  {
+    "revision": "26b2bf34d0232c57c23d",
+    "url": "/jiujitsutimer/static/js/2.a2b4a8e3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/jiujitsutimer/static/js/2.a2b4a8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7b787452f161c7084f",
+    "url": "/jiujitsutimer/static/js/main.bccf879b.chunk.js"
+  },
+  {
+    "revision": "799a573cff7686b45d6a",
+    "url": "/jiujitsutimer/static/js/runtime-main.b6b7181e.js"
+  }
+]);
